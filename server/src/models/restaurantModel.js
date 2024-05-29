1230b99224f3fa25cf14511c15bc3a9e6bd23c84
John Doe <yusay.safadi@gmail.com>
2024-05-29 02:24:23 +0200

    Add comment to addRestaurant function

--- server/src/models/restaurantModel.js
+++ server/src/models/restaurantModel.js
@@ -4,7 +4,9 @@ const db = require('../db');
 function getAllRestaurants() {
     return db.any('SELECT * FROM restaurants');
 }
-// Add a new restaurant to database
+// Adds a new restaurant to the database
+// Params: restaurantData - Object containing name, location_id, and description of the restaurant
+// Returns: Promise resolving to the new restaurant entry
 function addRestaurant(restaurantData) {
     return db.none('INSERT INTO restaurants (name,location_id,description) VALUES ($1, $2, $3)',
         [restaurantData.name,restaurantData.location_id, restaurantData.description]);
