3c0e4c7bc1b303c0a8fa42fb36e9d0df61947b3b
John Doe <yusay.safadi@gmail.com>
2024-05-29 17:20:27 +0200

    Fix typo in restaurantRoutes.js

--- server/src/routes/restaurantRoutes.js
+++ server/src/routes/restaurantRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const restaurantsController = require('../controllers/restaurantsController');
+const restaurantsController = require('../controllers/restaurantController');
 
 router.get('/restaurants', restaurantsController.getAllRestaurants);
 router.post('/restaurants', restaurantsController.addRestaurant);
