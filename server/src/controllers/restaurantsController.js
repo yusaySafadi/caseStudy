b0cbbbcedc6584b0d6d46af38c0c1cd49d808106
John Doe <yusay.safadi@gmail.com>
2024-05-29 02:13:20 +0200

    Add Restaurant controller with CRUD operations

---
+++ server/src/controllers/restaurantsController.js
@@ -0,0 +1,24 @@
+const restaurantModel = require("../models/restaurantModel");
+
+async function getAllRestaurants(req, res) {
+    try{
+        const data = await restaurantModel.getAllRestaurants();
+        res.status(200).json(data);
+    } catch (err) {
+        res.status(500).send('Failed to retrieve restaurants');
+    }
+}
+
+async function addRestaurant(req, res) {
+    try {
+        await restaurantModel.addRestaurant(req.body);
+        res.status(200).send('Successfully added restaurant');
+    } catch (err){
+        res.status(500).send('Failed to add restaurant');
+    }
+}
+
+module.exports = {
+    getAllRestaurants,
+    addRestaurant
+}
