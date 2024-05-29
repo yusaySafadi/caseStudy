fbaa08585bf332e4ed3b41bbe074f622a09a851a
John Doe <yusay.safadi@gmail.com>
2024-05-29 17:20:49 +0200

    Add locationRoutes to app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+//import routes
 const restaurantRoutes = require('./routes/restaurantRoutes');
+const locationRoutes = require('./routes/locationRoutes');
+
 app.use(express.json()) //For parsing application/json
+
 app.use('/api', restaurantRoutes)
-console.log(typeof process.env.DB_PASSWORD);
+app.use('/api', locationRoutes)
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
