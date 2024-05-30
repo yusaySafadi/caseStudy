643750f4445226863e76e4d2f1e7658345e29d45
John Doe <yusay.safadi@gmail.com>
2024-05-30 02:37:20 +0200

    Add menu and menuitem routes to app

--- server/src/app.js
+++ server/src/app.js
@@ -5,11 +5,15 @@ const PORT = process.env.PORT || 3000;
 const restaurantRoutes = require('./routes/restaurantRoutes');
 const locationRoutes = require('./routes/locationRoutes');
 const cuisineRoutes = require('./routes/cuisineRoutes');
+const menuRoutes = require('./routes/menuRoutes')
+const menuItemRoutes  = require('./routes/menuItemRoutes')
 app.use(express.json()) //For parsing application/json
 
 app.use('/api', restaurantRoutes)
 app.use('/api', locationRoutes)
 app.use('/api', cuisineRoutes)
+app.use('/api', menuRoutes)
+app.use('/api', menuItemRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
