3bf7870bcbcd7847d2b45489f1b9127f60f1236f
John Doe <yusay.safadi@gmail.com>
2024-06-04 01:19:10 +0200

    Add RestaurantCard component

---
+++ client/src/categoryImages.js
@@ -0,0 +1,9 @@
+
+const categoryImages = {
+    asian: '/public/asian.jpg',
+    mediterranean: '/public/mediterranean.jpg',
+    italian: '/public/italian.jpg',
+    american:'/public/american.jpg',
+    german: '/public/german.jpg'
+};
+export default categoryImages;
---
+++ client/src/components/RestaurantCard/RestaurantCard.jsx
@@ -0,0 +1,25 @@
+
+import './RestaurantCard.css';
+import categoryImages from "../../categoryImages";
+
+function RestaurantCard({ restaurant, onSelect }) {
+    const randomImageId = Math.floor(Math.random() * 1000); // Generate a random image ID
+    const cuisineCategory = restaurant.cuisines[0]?.name.toLowerCase();
+    const imageUrl = categoryImages[cuisineCategory] || '../../assets/american.jpg'; // Use a default image if category doesn't match
+
+    return (
+        <div className="restaurant-card" onClick={() => onSelect(restaurant)}>
+            <div className="restaurant-card__image">
+                {/* Placeholder for the restaurant image */}
+                <img src={imageUrl} alt={restaurant.restaurant_name}/>
+            </div>
+            <div className="restaurant-card__info">
+            <h3 className="restaurant-card__name">{restaurant.restaurant_name}</h3>
+                <p className="restaurant-card__cuisine">{restaurant.cuisines.map(cuisine => cuisine.name).join(', ')}</p>
+                <p className="restaurant-card__description">{restaurant.description}</p>
+            </div>
+        </div>
+    );
+}
+
+export default RestaurantCard;
